90d5a744fed3967167f920f7a11d8468599c8a1b
xukui <xukui@wondersgroup.com>
2019-10-14 17:16:29 +0800

    init

--- easeui/src/main/java/com/hyphenate/easeui/module/inquiry/widget/EaseInquiryEndedMenu.java
+++ easeui/src/main/java/com/hyphenate/easeui/module/inquiry/widget/EaseInquiryEndedMenu.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 
@@ -22,34 +21,37 @@ import java.util.List;
 
 public class EaseInquiryEndedMenu extends LinearLayoutCompat {
 
-    private static final int SPAN_COUNT = 2;
-
     private RecyclerView recycler_menu;
 
+    private GridLayoutManager mLayoutManager;
     private EaseInquiryEndedMenuRecyclerAdapter mRecyclerAdapter;
+
+    private int mSpaceCount;
 
     public EaseInquiryEndedMenu(Context context) {
         super(context);
-        initData();
+        initData(context);
         initView(context);
-        setView(context);
+        setView();
     }
 
     public EaseInquiryEndedMenu(Context context, AttributeSet attrs) {
         super(context, attrs);
-        initData();
+        initData(context);
         initView(context);
-        setView(context);
+        setView();
     }
 
     public EaseInquiryEndedMenu(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        initData();
+        initData(context);
         initView(context);
-        setView(context);
+        setView();
     }
 
-    private void initData() {
+    private void initData(Context context) {
+        mSpaceCount = 2;
+        mLayoutManager = new GridLayoutManager(context, mSpaceCount);
         mRecyclerAdapter = new EaseInquiryEndedMenuRecyclerAdapter();
     }
 
@@ -60,52 +62,89 @@ public class EaseInquiryEndedMenu extends LinearLayoutCompat {
         recycler_menu = view.findViewById(R.id.recycler_menu);
     }
 
-    private void setView(Context context) {
-        GridLayoutManager layoutManager = new GridLayoutManager(context, SPAN_COUNT);
-        recycler_menu.setLayoutManager(layoutManager);
+    private void setView() {
+        recycler_menu.setLayoutManager(mLayoutManager);
         recycler_menu.addItemDecoration(new EaseGridItemDecoration());
         recycler_menu.setAdapter(mRecyclerAdapter);
     }
 
     public void setData(List<EaseInquiryEndedMenuItem> menuItems) {
+        int count = (menuItems == null ? 0 : menuItems.size());
+        mSpaceCount = count == 1 ? 1 : 2;
+
+        mLayoutManager.setSpanCount(mSpaceCount);
+        recycler_menu.setLayoutManager(mLayoutManager);
         mRecyclerAdapter.setData(menuItems);
     }
 
     class EaseGridItemDecoration extends RecyclerView.ItemDecoration {
 
         private Paint mPaint;
-        private int mDiverWidth;
-
-        private final Rect mBounds = new Rect();
+        private int mSpace;
+        private int mPadding;
 
         public EaseGridItemDecoration() {
-            mDiverWidth = DensityUtil.dp2px(1);
+            mSpace = DensityUtil.dp2px(1);
+            mPadding = DensityUtil.dp2px(10);
 
             mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             mPaint.setColor(ContextCompatUtil.getColor(R.color.ease_bg));
             mPaint.setStyle(Paint.Style.FILL);
-            mPaint.setStrokeWidth(mDiverWidth);
+            mPaint.setStrokeWidth(mSpace);
         }
 
         @Override
         public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
             int position = parent.getChildAdapterPosition(view);
-            int column = position % SPAN_COUNT;
+            int row = position / mSpaceCount;
+            int column = position % mSpaceCount;
 
-            outRect.left = column * mDiverWidth / SPAN_COUNT;
-            outRect.right = mDiverWidth - (column + 1) * mDiverWidth / SPAN_COUNT;
+            if (column < mSpaceCount - 1) {
+                outRect.right = mSpace;
+            }
+
+            if (row > 0) {
+                outRect.top = mSpace;
+            }
+        }
+
+        private int getRowCount(RecyclerView parent) {
+            int count = parent.getChildCount();
+
+            int rowCount = (count / mSpaceCount);
+            if (rowCount * mSpaceCount < count) {
+                rowCount++;
+            }
+
+            return rowCount;
         }
 
         @Override
         public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
             super.onDraw(c, parent, state);
+            int count = parent.getChildCount();
+
+            if (count <= 1) {
+                return;
+            }
+
+            int rowCount = getRowCount(parent);
+            float itemWidth = 1f * parent.getWidth() / mSpaceCount;
+            float itemHeight = 1f * parent.getHeight() / rowCount;
+
             for (int i = 0; i < parent.getChildCount(); i++) {
-                View view = parent.getChildAt(i);
-                parent.getDecoratedBoundsWithMargins(view, mBounds);
+                int row = i / mSpaceCount;
+                int column = i % mSpaceCount;
 
-                Log.e("ddd", mBounds.toString());
-
-                c.drawRect(mBounds, mPaint);
+                c.save();
+                c.translate(column * itemWidth, row * itemHeight);
+                if (column < mSpaceCount - 1) {
+                    c.drawRect(itemWidth - mSpace, mPadding, itemWidth, itemHeight - mPadding, mPaint);
+                }
+                if (row > 0) {
+                    c.drawRect(mPadding, 0, itemWidth - mPadding, mSpace, mPaint);
+                }
+                c.restore();
             }
         }
 
