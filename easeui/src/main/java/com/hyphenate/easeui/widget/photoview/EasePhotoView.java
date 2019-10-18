b0158c3dfee212af2290cfae68c49a5a0686bce5
xukui <xukui@wondersgroup.com>
2019-10-18 09:53:39 +0800

    init

--- easeui/src/main/java/com/hyphenate/easeui/widget/photoview/EasePhotoView.java
+++ easeui/src/main/java/com/hyphenate/easeui/widget/photoview/EasePhotoView.java
@@ -4,72 +4,72 @@ import android.content.Context;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.widget.ImageView;
 
 import com.hyphenate.easeui.widget.photoview.PhotoViewAttacher.OnMatrixChangedListener;
 import com.hyphenate.easeui.widget.photoview.PhotoViewAttacher.OnPhotoTapListener;
 import com.hyphenate.easeui.widget.photoview.PhotoViewAttacher.OnViewTapListener;
 
-public class EasePhotoView extends ImageView implements IPhotoView {
+public class EasePhotoView extends AppCompatImageView implements IPhotoView {
 
-	private final PhotoViewAttacher mAttacher;
+    private final PhotoViewAttacher mAttacher;
 
-	private ScaleType mPendingScaleType;
+    private ScaleType mPendingScaleType;
 
-	public EasePhotoView(Context context) {
-		this(context, null);
-	}
+    public EasePhotoView(Context context) {
+        this(context, null);
+    }
 
-	public EasePhotoView(Context context, AttributeSet attr) {
-		this(context, attr, 0);
-	}
-	
-	public EasePhotoView(Context context, AttributeSet attr, int defStyle) {
-		super(context, attr, defStyle);
-		super.setScaleType(ScaleType.MATRIX);
-		mAttacher = new PhotoViewAttacher(this);
+    public EasePhotoView(Context context, AttributeSet attr) {
+        this(context, attr, 0);
+    }
 
-		if (null != mPendingScaleType) {
-			setScaleType(mPendingScaleType);
-			mPendingScaleType = null;
-		}
-	}
+    public EasePhotoView(Context context, AttributeSet attr, int defStyle) {
+        super(context, attr, defStyle);
+        super.setScaleType(ScaleType.MATRIX);
+        mAttacher = new PhotoViewAttacher(this);
 
-	@Override
-	public boolean canZoom() {
-		return mAttacher.canZoom();
-	}
+        if (mPendingScaleType != null) {
+            setScaleType(mPendingScaleType);
+            mPendingScaleType = null;
+        }
+    }
 
-	@Override
-	public RectF getDisplayRect() {
-		return mAttacher.getDisplayRect();
-	}
+    @Override
+    public boolean canZoom() {
+        return mAttacher.canZoom();
+    }
 
-	@Override
-	public float getMinScale() {
-		return mAttacher.getMinScale();
-	}
+    @Override
+    public RectF getDisplayRect() {
+        return mAttacher.getDisplayRect();
+    }
 
-	@Override
-	public float getMidScale() {
-		return mAttacher.getMidScale();
-	}
+    @Override
+    public float getMinScale() {
+        return mAttacher.getMinScale();
+    }
 
-	@Override
-	public float getMaxScale() {
-		return mAttacher.getMaxScale();
-	}
+    @Override
+    public float getMidScale() {
+        return mAttacher.getMidScale();
+    }
 
-	@Override
-	public float getScale() {
-		return mAttacher.getScale();
-	}
+    @Override
+    public float getMaxScale() {
+        return mAttacher.getMaxScale();
+    }
 
-	@Override
-	public ScaleType getScaleType() {
-		return mAttacher.getScaleType();
-	}
+    @Override
+    public float getScale() {
+        return mAttacher.getScale();
+    }
+
+    @Override
+    public ScaleType getScaleType() {
+        return mAttacher.getScaleType();
+    }
 
     @Override
     public void setAllowParentInterceptOnEdge(boolean allow) {
@@ -77,88 +77,88 @@ public class EasePhotoView extends ImageView implements IPhotoView {
     }
 
     @Override
-	public void setMinScale(float minScale) {
-		mAttacher.setMinScale(minScale);
-	}
+    public void setMinScale(float minScale) {
+        mAttacher.setMinScale(minScale);
+    }
 
-	@Override
-	public void setMidScale(float midScale) {
-		mAttacher.setMidScale(midScale);
-	}
+    @Override
+    public void setMidScale(float midScale) {
+        mAttacher.setMidScale(midScale);
+    }
 
-	@Override
-	public void setMaxScale(float maxScale) {
-		mAttacher.setMaxScale(maxScale);
-	}
+    @Override
+    public void setMaxScale(float maxScale) {
+        mAttacher.setMaxScale(maxScale);
+    }
 
-	@Override
-	// setImageBitmap calls through to this method
-	public void setImageDrawable(Drawable drawable) {
-		super.setImageDrawable(drawable);
-		if (null != mAttacher) {
-			mAttacher.update();
-		}
-	}
+    @Override
+    public void setImageDrawable(Drawable drawable) {
+        super.setImageDrawable(drawable);
+        if (null != mAttacher) {
+            mAttacher.update();
+        }
+    }
 
-	@Override
-	public void setImageResource(int resId) {
-		super.setImageResource(resId);
-		if (null != mAttacher) {
-			mAttacher.update();
-		}
-	}
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        if (null != mAttacher) {
+            mAttacher.update();
+        }
+    }
 
-	@Override
-	public void setImageURI(Uri uri) {
-		super.setImageURI(uri);
-		if (null != mAttacher) {
-			mAttacher.update();
-		}
-	}
+    @Override
+    public void setImageURI(Uri uri) {
+        super.setImageURI(uri);
+        if (null != mAttacher) {
+            mAttacher.update();
+        }
+    }
 
-	@Override
-	public void setOnMatrixChangeListener(OnMatrixChangedListener listener) {
-		mAttacher.setOnMatrixChangeListener(listener);
-	}
+    @Override
+    public void setOnMatrixChangeListener(OnMatrixChangedListener listener) {
+        mAttacher.setOnMatrixChangeListener(listener);
+    }
 
-	@Override
-	public void setOnLongClickListener(OnLongClickListener l) {
-		mAttacher.setOnLongClickListener(l);
-	}
+    @Override
+    public void setOnLongClickListener(OnLongClickListener l) {
+        mAttacher.setOnLongClickListener(l);
+    }
 
-	@Override
-	public void setOnPhotoTapListener(OnPhotoTapListener listener) {
-		mAttacher.setOnPhotoTapListener(listener);
-	}
+    @Override
+    public void setOnPhotoTapListener(OnPhotoTapListener listener) {
+        mAttacher.setOnPhotoTapListener(listener);
+    }
 
-	@Override
-	public void setOnViewTapListener(OnViewTapListener listener) {
-		mAttacher.setOnViewTapListener(listener);
-	}
+    @Override
+    public void setOnViewTapListener(OnViewTapListener listener) {
+        mAttacher.setOnViewTapListener(listener);
+    }
 
-	@Override
-	public void setScaleType(ScaleType scaleType) {
-		if (null != mAttacher) {
-			mAttacher.setScaleType(scaleType);
-		} else {
-			mPendingScaleType = scaleType;
-		}
-	}
+    @Override
+    public void setScaleType(ScaleType scaleType) {
+        if (null != mAttacher) {
+            mAttacher.setScaleType(scaleType);
 
-	@Override
-	public void setZoomable(boolean zoomable) {
-		mAttacher.setZoomable(zoomable);
-	}
+        } else {
+            mPendingScaleType = scaleType;
+        }
+    }
 
-	@Override
-	public void zoomTo(float scale, float focalX, float focalY) {
-		mAttacher.zoomTo(scale, focalX, focalY);
-	}
+    @Override
+    public void setZoomable(boolean zoomable) {
+        mAttacher.setZoomable(zoomable);
+    }
 
-	@Override
-	protected void onDetachedFromWindow() {
-		mAttacher.cleanup();
-		super.onDetachedFromWindow();
-	}
+    @Override
+    public void zoomTo(float scale, float focalX, float focalY) {
+        mAttacher.zoomTo(scale, focalX, focalY);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        mAttacher.cleanup();
+        super.onDetachedFromWindow();
+    }
 
 }
