7ce8b6ce8db3a6cbce674fdac3e29221702398d1
xukui <xukui@wondersgroup.com>
2019-11-04 10:22:59 +0800

    init

--- easeui/src/main/java/com/hyphenate/easeui/module/base/widget/message/row/EaseChatRow.java
+++ easeui/src/main/java/com/hyphenate/easeui/module/base/widget/message/row/EaseChatRow.java
@@ -197,6 +197,13 @@ public abstract class EaseChatRow extends LinearLayout {
                 }
             }
         }
+
+
+        //test
+        if(tv_username != null) {
+            tv_username.setVisibility(View.VISIBLE);
+        }
+        //end
     }
 
     private void setClickListener() {
