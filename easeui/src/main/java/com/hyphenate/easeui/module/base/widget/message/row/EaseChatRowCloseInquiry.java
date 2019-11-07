32283d356660506347e6a9fd8a928cddb80fa949
xukui <xukui@wondersgroup.com>
2019-11-07 09:07:04 +0800

    init

--- easeui/src/main/java/com/hyphenate/easeui/module/base/widget/message/row/EaseChatRowCloseInquiry.java
+++ easeui/src/main/java/com/hyphenate/easeui/module/base/widget/message/row/EaseChatRowCloseInquiry.java
@@ -4,15 +4,11 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
 
 import com.hyphenate.chat.EMMessage;
-import com.hyphenate.chat.EMTextMessageBody;
 import com.hyphenate.easeui.R;
 
 public class EaseChatRowCloseInquiry extends EaseChatRow {
-
-    private TextView tv_content;
 
     public EaseChatRowCloseInquiry(Context context, EMMessage message, int position, BaseAdapter adapter) {
         super(context, message, position, adapter);
@@ -25,7 +21,6 @@ public class EaseChatRowCloseInquiry extends EaseChatRow {
 
     @Override
     protected void onFindViewById(View view) {
-        tv_content = view.findViewById(R.id.tv_content);
     }
 
     @Override
@@ -34,8 +29,6 @@ public class EaseChatRowCloseInquiry extends EaseChatRow {
 
     @Override
     protected void onSetUpView() {
-        EMTextMessageBody body = (EMTextMessageBody) mMessage.getBody();
-        tv_content.setText(body.getMessage());
     }
 
 }
